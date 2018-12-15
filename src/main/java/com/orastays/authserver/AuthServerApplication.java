a44654bac29c498e8e84b4f809f6c09223e25659
avi3s <avirup.pal@gmail.com>
2018-12-16 01:29:44 +0530

    last changes

--- src/main/java/com/orastays/authserver/AuthServerApplication.java
+++ src/main/java/com/orastays/authserver/AuthServerApplication.java
@@ -14,7 +14,6 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-//import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
