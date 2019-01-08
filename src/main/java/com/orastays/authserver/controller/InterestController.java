08aef3e0ff5b9f6dac66d85c63608fa1f3f8d719
sudeep-ora <45230927+sudeep-ora@users.noreply.github.com>
2019-01-08 17:01:37 +0530

    Update InterestController.java

--- src/main/java/com/orastays/authserver/controller/InterestController.java
+++ src/main/java/com/orastays/authserver/controller/InterestController.java
@@ -46,7 +46,7 @@ public class InterestController extends BaseController {
 			@ApiResponse(code = 321, message = "Please give User Token"),
 			@ApiResponse(code = 322, message = "Invalid user Token"),
 			@ApiResponse(code = 1010, message = "User is Inactive")})
-	public ResponseEntity<ResponseModel> fetchInterests(@RequestParam(value = "hostToken", required = true) String hostToken) {
+	public ResponseEntity<ResponseModel> fetchInterests(@RequestParam(value = "userToken", required = true) String userToken) {
 
 		if (logger.isInfoEnabled()) {
 			logger.info("fetchInterests -- START");
@@ -55,7 +55,7 @@ public class InterestController extends BaseController {
 		ResponseModel responseModel = new ResponseModel();
 		Util.printLog(responseModel, AuthConstant.OUTGOING, "Fetch Interests", request);
 		try {
-			List<InterestModel> interestModels = interestService.fetchInterests(hostToken);
+			List<InterestModel> interestModels = interestService.fetchInterests(userToken);
 			responseModel.setResponseBody(interestModels);
 			responseModel.setResponseCode(messageUtil.getBundle(AuthConstant.COMMON_SUCCESS_CODE));
 			responseModel.setResponseMessage(messageUtil.getBundle(AuthConstant.COMMON_SUCCESS_MESSAGE));
