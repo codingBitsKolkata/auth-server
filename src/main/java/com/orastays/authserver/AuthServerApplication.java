f02d12a7f2be4c55fe29f1a1542dc9ac7331d122
Sudeep Chhetri <Sudeep.Chhetri@oraweb.com>
2019-03-23 21:04:04 +0530

    Update AuthServerApplication.java

--- src/main/java/com/orastays/authserver/AuthServerApplication.java
+++ src/main/java/com/orastays/authserver/AuthServerApplication.java
@@ -58,7 +58,7 @@ public class AuthServerApplication extends WebMvcConfigurerAdapter {
 	}
 
 	@Bean
-	@LoadBalanced
+	//@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
